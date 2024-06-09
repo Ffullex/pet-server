9be4358b9dfa0705915f9f7c387c4861e7bb0644
ffullex <tomanovsky.yv@dclouds.ru>
2024-06-09 22:03:11 +0700

    pet-server: фильтрация, кэш

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -5,7 +5,28 @@ const recordController = require('../../controllers/recordController')
 
 const router = express.Router()
 
-
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ */
 router.get('/', workoutController.getAllWorkouts)
 
 router.get('/:workoutId', workoutController.getOneWorkout)
