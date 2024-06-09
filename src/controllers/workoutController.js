c45d9014c30f080e5bdbb5e596f1b9d4c308aa18
ffullex <tomanovsky.yv@dclouds.ru>
2024-06-09 21:44:04 +0700

    pet-server: обработка всех ошибок

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,8 +1,15 @@
 const workoutService = require('../services/workoutService')
 
 const getAllWorkouts = (req, res) => {
-  const allWorkouts = workoutService.getAllWorkouts()
-  res.send({ status: 'OK', data: allWorkouts })
+  try {
+    const allWorkouts = workoutService.getAllWorkouts()
+    res.send({ status: 'OK', data: allWorkouts })
+  } catch (error) {
+    res.status(error?.status || 500).send({
+      status: 'FAILED',
+      data: { error: error?.message || error },
+    })
+  }
 }
 
 const getOneWorkout = (req, res) => {
@@ -10,10 +17,25 @@ const getOneWorkout = (req, res) => {
     params: { workoutId },
   } = req
   if ( !workoutId) {
-    return
+    res.status(400).send({
+      status: 'FAILED',
+      data: {
+        error:
+          'Parameter \':workoutId\' can not be empty',
+      },
+    })
   }
-  const workout = workoutService.getOneWorkout(workoutId)
-  res.send({ status: 'OK', data: workout })
+  try {
+    const workout = workoutService.getOneWorkout(
+      workoutId,
+    )
+    res.send({ status: 'OK', data: workout })
+  } catch (error) {
+    res.status(error?.status || 500).send({
+      status: 'FAILED',
+      data: { error: error?.message || error },
+    })
+  }
 }
 
 const createNewWorkout = (req, res) => {
@@ -41,7 +63,6 @@ const createNewWorkout = (req, res) => {
     exercises: body.exercises,
     trainerTips: body.trainerTips,
   }
-
   try {
     const createdWorkout = workoutService.createNewWorkout(
       newWorkout,
@@ -64,13 +85,26 @@ const updateOneWorkout = (req, res) => {
     params: { workoutId },
   } = req
   if ( !workoutId) {
-    return
+    res.status(400).send({
+      status: 'FAILED',
+      data: {
+        error:
+          'Parameter \':workoutId\' can not be empty',
+      },
+    })
   }
-  const updatedWorkout = workoutService.updateOneWorkout(
-    workoutId,
-    body,
-  )
-  res.send({ status: 'OK', data: updatedWorkout })
+  try {
+    const updatedWorkout = workoutService.updateOneWorkout(
+      workoutId,
+      body,
+    )
+    res.send({ status: 'OK', data: updatedWorkout })
+  } catch (error) {
+    res.status(error?.status || 500).send({
+      status: 'FAILED',
+      data: { error: error?.message || error },
+    })
+  }
 }
 
 const deleteOneWorkout = (req, res) => {
@@ -78,10 +112,23 @@ const deleteOneWorkout = (req, res) => {
     params: { workoutId },
   } = req
   if ( !workoutId) {
-    return
+    res.status(400).send({
+      status: 'FAILED',
+      data: {
+        error:
+          'Parameter \':workoutId\' can not be empty',
+      },
+    })
   }
-  workoutService.deleteOneWorkout(workoutId)
-  res.status(204).send({ status: 'OK' })
+  try {
+    workoutService.deleteOneWorkout(workoutId)
+    res.status(204).send({ status: 'OK' })
+  } catch (error) {
+    res.status(error?.status || 500).send({
+      status: 'FAILED',
+      data: { error: error?.message || error },
+    })
+  }
 }
 
 module.exports = {
@@ -90,4 +137,5 @@ module.exports = {
   createNewWorkout,
   updateOneWorkout,
   deleteOneWorkout,
+  getRecordsForWorkout,
 }
